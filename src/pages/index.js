4d5dec062864576f9606e99c8066d94977c20d6f
rjclark3 <rodneyclark11194@gmail.com>
2019-08-12 10:57:20 -0400

    First commit for the Stark's Gatsby Site, Stripe Product button functionality.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,56 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+class Button extends React.Component {
+
+  componentDidMount() {
+    this.stripe = window.Stripe(process.env.STRIPE_SECRET_KEY);
+  }
+
+  render() {
+    return (
+      <form onSubmit={event => {
+        event.preventDefault();
+        this.stripe
+          .redirectToCheckout({
+            items: [{ sku: 'sku_FZHfRgXFC38haf', quantity: 1 }],
+
+            // Do not rely on the redirect to the successUrl for fulfilling
+            // purchases, customers may not always reach the success_url after
+            // a successful payment.
+            // Instead use one of the strategies described in
+            // https://stripe.com/docs/payments/checkout/fulfillment
+            successUrl: 'http://localhost:8000/success',
+            cancelUrl: 'http://localhost:8000/canceled',
+          })
+          .then(function (result) {
+            if (result.error) {
+              // If `redirectToCheckout` fails due to a browser or network
+              // error, display the localized error message to your customer.
+              var displayError = document.getElementById('error-message');
+              displayError.textContent = result.error.message;
+            }
+          });
+      }
+      }>
+        <button type="submit" >Buy Prodkt</button>
+      </form>
+    );
+  }
+}
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <script src="https://js.stripe.com/v3/"></script>
+    <SEO title="Stark's Plastics" />
+    <h1>Welcome to Stark's</h1>
+    <p>This is our new website!.</p>
+    <Button />
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
